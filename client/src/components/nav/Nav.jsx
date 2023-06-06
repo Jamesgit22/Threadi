2d12092f6c8cae2606ab9e19aebc21489e47aa4c
rahargrave <128389433+rahargrave@users.noreply.github.com>
2023-06-06 14:06:52 -0400

    set up desktop buttons

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './Nav.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Nav.css";
 
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,83 +15,90 @@ export default function Nav() {
         setIsMobile(false);
       }
     };
-  
+
     window.addEventListener("resize", handleResize);
     handleResize();
-
   }, []);
-  
+
   const toggleMenu = () => {
     setIsOpen((open) => !open);
   };
   return (
     <>
-      <div className='container-fluid'>
-        <div className='row'>
-          <nav id='nav-container' className='col-12'>
-            <div id='logo-container' className='col-4'>
-              <h2 id='nav-logo' className='light-txt'>Logo</h2>
+      <div className="header container-fluid">
+        <div className="row">
+          <nav id="nav-container" className="col-12">
+            <div id="logo-container" className="col-4">
+              <h2 id="nav-logo" className="light-txt">
+                THREADI
+              </h2>
             </div>
             {isMobile && ( // add conditional statement here
-            <div className='mobile'>
-              <div id='nav-links' className='col-8'>
-                <div id='hamburger-icon' onClick={() => toggleMenu()}>
-                  <div className={`bar1 bars ${isOpen ? 'open' : ''}`}></div>
-                  <div className={`bar2 bars ${isOpen ? 'open' : ''}`}></div>
-                  <div className={`bar3 bars ${isOpen ? 'open' : ''}`}></div>
-                </div>
-                <div
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1 }}
-                  transition={{ duration: 0.2 }}
-                  exit={{ opacity: 0 }}
-                  id='mobile-menu'
-                  className={`${isOpen ? 'open' : ''}`}
-                >
-                  <button
-                    className='mobile-nav-btns'
-                    href='/'
-                    onClick={() => {
-                      // handleViewChange('Projects');
-                      toggleMenu();
-                    }}
-                  >
-                    My Lists
-                  </button>
-                  <button
-                    className='mobile-nav-btns'
-                    href='/'
-                    onClick={() => {
-                      // handleViewChange('About');
-                      toggleMenu();
-                    }}
-                  >
-                    Friends
-                  </button>
-                  <button
-                    className='mobile-nav-btns'
-                    href='/'
-                    onClick={() => {
-                      // handleViewChange('Contact');
-                      toggleMenu();
-                    }}
-                  >
-                    Following
-                  </button>
-                  <button
-                    className='mobile-nav-btns'
-                    href='/'
-                    onClick={() => {
-                      // handleViewChange('Home');
-                      toggleMenu();
-                    }}
-                  >
-                    Logout
-                  </button>
+              <div className="mobile">
+                <div id="nav-links" className="col-8">
+                  <div id="hamburger-icon" onClick={() => toggleMenu()}>
+                    <div className={`bar1 bars ${isOpen ? "open" : ""}`}></div>
+                    <div className={`bar2 bars ${isOpen ? "open" : ""}`}></div>
+                    <div className={`bar3 bars ${isOpen ? "open" : ""}`}></div>
+                  </div>
+                  <div id="mobile-menu" className={`${isOpen ? "open" : ""}`}>
+                    <button
+                      className="mobile-nav-btns"
+                      href="/"
+                      onClick={() => {
+                        // handleViewChange('Projects');
+                        toggleMenu();
+                      }}
+                    >
+                      My Lists
+                    </button>
+                    <button
+                      className="mobile-nav-btns"
+                      href="/"
+                      onClick={() => {
+                        // handleViewChange('About');
+                        toggleMenu();
+                      }}
+                    >
+                      Friends
+                    </button>
+                    <button
+                      className="mobile-nav-btns"
+                      href="/"
+                      onClick={() => {
+                        // handleViewChange('Contact');
+                        toggleMenu();
+                      }}
+                    >
+                      Following
+                    </button>
+                    <button
+                      className="mobile-nav-btns"
+                      href="/"
+                      onClick={() => {
+                        // handleViewChange('Home');
+                        toggleMenu();
+                      }}
+                    >
+                      Logout
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
+            {!isMobile && ( // add conditional statement here
+              <div className="desktop">
+                <div className="desktopNav">
+                  <button className="desktop-nav-btns">Social</button>
+                  <button className="desktop-nav-btns">Profile</button>
+                  <button className="desktop-nav-btns">Browse</button>
+                </div>
+                <div className="desktopSignOn">
+                  <button className="desktop-signin-btns">Sign In</button>
+                  <button className="desktop-signup-btns">Sign Up</button>
+                </div>
+              </div>
+            )}
           </nav>
         </div>
       </div>
