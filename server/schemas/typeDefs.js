e66273b0ce4be2670f97f0f912ca2372a56ac0ad
Jamesgit22 <jschroeder97@icloud.com>
2023-06-06 12:51:32 -0500

    Added Starter typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,26 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID!
-    username: String
-    password: String
-    admin: Boolean
+    username: String!
+    password: String!
+  }
+
+  type Review {
+    _id: ID!
+    author: User
+    text: String
+    rating: Int
+    likes: Int
+    comments: [Comment]
+    dateWatched: Date
+  }
+
+  type Comment {
+    _id: ID
+    author: User
+    text: String
+    likes: Int
+    comments: [Comment]
   }
 
   type Auth {
@@ -13,12 +30,24 @@ const typeDefs = gql`
     user: User
   }
 
+  type Thread {
+    _id: ID!
+    title: String!
+    reviews: [Review]
+  }
+
   type Query {
+    reviews: [Review]
+    comments: [Comment]
+    threads: [Thread]
+    friends: [User]
+    
 
   }
 
   type Mutation {
     login(username: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
