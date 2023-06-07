9d5afe5d91237bd4e4d4deced4d5bd70396ade0f
Jamesgit22 <jschroeder97@icloud.com>
2023-06-07 13:15:15 -0500

    Resolving conflict bug

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,52 +1,72 @@
-import React from 'react'
+import React from 'react';
 import './login.css';
 
 export default function Login() {
   return (
     <>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-12 text-center pt-5 pb-1 mt-5">
-                    <h2 id="login">Welcome<br></br>back</h2>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <form>
-                        <div className="row justify-content-center pb-3">
-                            <div className="col-10">
-                                <input className='inputs' type="text" placeholder='username'/>                                
-                            </div>
-                        </div>
-                        <div className="row  justify-content-center">
-                            <div className="col-10">
-                                <input className='inputs' type="text" placeholder='password'/>                                
-                            </div>
-                        </div>
-                        <div className="row  justify-content-center">
-                            <div className="col-8 text-center pt-4">
-                                <button id='login-btn' type='submit'>Login</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div className="row pt-5">
-                <div className="col-12 d-flex justify-content-center">
-                    <div className="col-4 text-center">
-                        <button className='nav-btns' id='go-back-btn'>Go Back</button>
-                    </div>
-                    <div className="col-4 text-center">
-                        <button className='nav-btns' id='sign-up-btn'>Sign Up</button>
-                    </div>
-                </div>
-            </div>
-            <div className="row pt-4">
-                <div className="col-12 text-center">
-                    <a href=""><p id='forgot-pass' className='mb-5'>Forgot Password?</p></a>
-                </div>
-            </div>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-12 text-center pt-5 pb-1 mt-5'>
+            <h2 id='login'>
+              Welcome<br></br>back
+            </h2>
+          </div>
         </div>
+        <div className='row'>
+          <div className='col-12'>
+            <form>
+              <div className='row justify-content-center pb-3'>
+                <div className='col-10'>
+                  <input
+                    className='inputs'
+                    type='text'
+                    placeholder='username'
+                  />
+                </div>
+              </div>
+              <div className='row  justify-content-center'>
+                <div className='col-10'>
+                  <input
+                    className='inputs'
+                    type='text'
+                    placeholder='password'
+                  />
+                </div>
+              </div>
+              <div className='row  justify-content-center'>
+                <div className='col-8 text-center pt-4'>
+                  <button id='login-btn' type='submit'>
+                    Login
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className='row pt-5'>
+          <div className='col-12 d-flex justify-content-center'>
+            <div className='col-4 text-center'>
+              <button className='nav-btns' id='go-back-btn'>
+                Go Back
+              </button>
+            </div>
+            <div className='col-4 text-center'>
+              <button className='nav-btns' id='sign-up-btn'>
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='row pt-4'>
+          <div className='col-12 text-center'>
+            <a href=''>
+              <p id='forgot-pass' className='mb-5'>
+                Forgot Password?
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import "./Nav.css";
-import Search from "../search/Search";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './Nav.css';
+import Search from '../search/Search';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 
+// resolve conflicts.
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -19,7 +20,7 @@ export default function Nav() {
       }
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
     handleResize();
   }, []);
 
@@ -29,31 +30,33 @@ export default function Nav() {
 
   return (
     <>
-      <div className="header container-fluid">
-        <div className="row">
-          <nav id="nav-container" className="col-12">
-            <div id="logo-container" className="col-4">
-              <h2 id="nav-logo" className="light-txt">
+      <div className='header container-fluid'>
+        <div className='row'>
+          <nav id='nav-container' className='col-12'>
+            <div id='logo-container' className='col-4'>
+              <h2 id='nav-logo' className='light-txt'>
                 THREADI
               </h2>
             </div>
 
             {isMobile && (
-              <div className="mobile">
-                <div id="nav-links" className="col-8">
-                  <div id="hamburger-icon" onClick={() => toggleMenu()}>
-                    <div className={`bar1 bars ${isOpen ? "open" : ""}`}></div>
-                    <div className={`bar2 bars ${isOpen ? "open" : ""}`}></div>
-                    <div className={`bar3 bars ${isOpen ? "open" : ""}`}></div>
+              <div className='mobile'>
+                <div id='nav-links' className='col-8'>
+                  <div id='hamburger-icon' onClick={() => toggleMenu()}>
+                    <div className={`bar1 bars ${isOpen ? 'open' : ''}`}></div>
+                    <div className={`bar2 bars ${isOpen ? 'open' : ''}`}></div>
+                    <div className={`bar3 bars ${isOpen ? 'open' : ''}`}></div>
                   </div>
                   <motion.div
-                  initial={{opacity: 0}}
-                  whileInView={{opacity: 1}}
-                  transition={{duration: 0.2}}
-                  id="mobile-menu" className={`${isOpen ? "open" : ""}`}>
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.2 }}
+                    id='mobile-menu'
+                    className={`${isOpen ? 'open' : ''}`}
+                  >
                     <button
-                      className="mobile-nav-btns"
-                      href="/"
+                      className='mobile-nav-btns'
+                      href='/'
                       onClick={() => {
                         // handleViewChange('Projects');
                         toggleMenu();
@@ -62,8 +65,8 @@ export default function Nav() {
                       My Lists
                     </button>
                     <button
-                      className="mobile-nav-btns"
-                      href="/"
+                      className='mobile-nav-btns'
+                      href='/'
                       onClick={() => {
                         // handleViewChange('About');
                         toggleMenu();
@@ -72,8 +75,8 @@ export default function Nav() {
                       Friends
                     </button>
                     <button
-                      className="mobile-nav-btns"
-                      href="/"
+                      className='mobile-nav-btns'
+                      href='/'
                       onClick={() => {
                         // handleViewChange('Contact');
                         toggleMenu();
@@ -82,8 +85,8 @@ export default function Nav() {
                       Following
                     </button>
                     <button
-                      className="mobile-nav-btns"
-                      href="/"
+                      className='mobile-nav-btns'
+                      href='/'
                       onClick={() => {
                         // handleViewChange('Home');
                         toggleMenu();
@@ -97,21 +100,21 @@ export default function Nav() {
             )}
 
             {!isMobile && (
-              <div className="desktop">
-                <div className="desktopNav">
-                  <button className="desktop-nav-btns">Social</button>
-                  <button className="desktop-nav-btns">Profile</button>
-                  <button className="desktop-nav-btns">Browse</button>
-                  <button className="desktop-nav-btns">
-                  <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+              <div className='desktop'>
+                <div className='desktopNav'>
+                  <button className='desktop-nav-btns'>Social</button>
+                  <button className='desktop-nav-btns'>Profile</button>
+                  <button className='desktop-nav-btns'>Browse</button>
+                  <button className='desktop-nav-btns'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
                   </button>
-                  <div className="desktop-nav-btns">
+                  <div className='desktop-nav-btns'>
                     <Search />
                   </div>
                 </div>
-                <div className="desktopSignOn">
-                  <button className="desktop-signin-btns">Sign In</button>
-                  <button className="desktop-signup-btns">Sign Up</button>
+                <div className='desktopSignOn'>
+                  <button className='desktop-signin-btns'>Sign In</button>
+                  <button className='desktop-signup-btns'>Sign Up</button>
                 </div>
               </div>
             )}
