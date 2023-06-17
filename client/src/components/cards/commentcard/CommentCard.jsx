5a6a18b900934db7f8aeaf3e2f6d9e5221b69485
Jamesgit22 <jschroeder97@icloud.com>
2023-06-17 17:53:13 -0500

    added starter code for thread card

---
+++ client/src/components/cards/commentcard/CommentCard.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './CommentCard.css';
+
+export default function CommentCard() {
+  return <></>;
+}
---
+++ client/src/components/cards/reviewcard/ReviewCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ReviewCard.css'
+
+export default function ReviewCard() {
+  return (
+    <>
+    
+    </>
+  )
+}
