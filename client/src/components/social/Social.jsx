6696f19c011ee5a19a2605a93dd5149d300db05e
Jamesgit22 <jschroeder97@icloud.com>
2023-06-17 17:23:27 -0500

    Started file structure for comments and imported boiler commentspage

--- client/src/components/social/Social.jsx
+++ client/src/components/social/Social.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useMutation } from '@apollo/client';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery, useLazyQuery } from '@apollo/client';
 import { GET_THREADS } from '../../utils/queries';
 import { SAVE_THREAD } from '../../utils/mutations';
+import { GET_COMMENT} from '../../utils/queries';
 import './Social.css';
 
 export default function Social() {
@@ -15,7 +15,7 @@ export default function Social() {
   const handleSaveThread = async (threadId) => {
     try {
       const { data } = await saveThread({
-        variables: { threadId }
+        variables: { threadId },
       });
 
       // Additional logic if needed
@@ -26,16 +26,28 @@ export default function Social() {
     }
   };
 
+// Go to comments page for seleted item
+  const handleCommentBtn = (e) => {
+    
+
+  }
+
   const formatTimestamp = (timestamp) => {
     let date = timestamp.split(' ');
-    return (date[1] + ' ' + date [2] + ' ' + date[3]);
-  }
+    return date[1] + ' ' + date[2] + ' ' + date[3];
+  };
 
   return (
     <>
-      <div id='social-main' className='container-fluid d-flex p-0 m-0 justify-content-center'>
+      <div
+        id='social-main'
+        className='container-fluid d-flex p-0 m-0 justify-content-center'
+      >
         <div className='col-12'>
-          <div id='social-container2' className='container-fluid justify-content-center'>
+          <div
+            id='social-container2'
+            className='container-fluid justify-content-center'
+          >
             <div id='social-background'>
               <div id='social-overlay' className='row justify-content-center'>
                 {/* section */}
@@ -49,7 +61,9 @@ export default function Social() {
                 <div className='row'>
                   <div className='col-12 text-center'>
                     <p id='social-msg'>
-                      Explore and stay up to date<br></br>with your <span id='social-friends'>FRIENDS</span> and the content<br></br> they love
+                      Explore and stay up to date<br></br>with your{' '}
+                      <span id='social-friends'>FRIENDS</span> and the content
+                      <br></br> they love
                     </p>
                   </div>
                 </div>
@@ -58,34 +72,67 @@ export default function Social() {
             {/* end section */}
             {/* section feed */}
             <div className='row p-0 justify-content-center'>
-              {threads.map((thread) => (
-                console.log(thread._id),
-                <div key={thread._id} id='feed-container' className='col-11 mb-3'>
-                  <div className='row p-0'>
-                    <div className='col-12 feed-username d-flex justify-content-between align-items-center'>
-                      <p className='social-username m-0 p-1'>A {thread.__typename} by {thread.author.username}</p>
-                      <img className='like-btn' src="/images/thumbs-up-regular.svg" alt="" />
+              {threads.map(
+                (thread) => (
+                  console.log(thread._id),
+                  (
+                    <div
+                      key={thread._id}
+                      id='feed-container'
+                      className='col-11 col-md-8 mb-3'
+                    >
+                      <div className='row p-0'>
+                        <div className='col-12 feed-username d-flex align-items-center'>
+                          <div className='col-11'>
+                            <p className='social-username m-0 p-1'>
+                              A {thread.__typename} by {thread.author.username}
+                            </p>
+                          </div>
+                          <div className='col-1 d-flex align-items-center justify-content-center'>
+                            <p className='likes-count'>{0}</p>
+                            <img
+                              className='like-btn'
+                              src='/images/thumbs-up-regular.svg'
+                              alt='like button'
+
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-12 d-flex pt-2 justify-content-between align-items-center'>
+                          <p>{thread.title}</p>
+                          <p>{formatTimestamp(thread.timestamp)}</p>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-12 content-container'>
+                          <p className='content-desc'>{thread.description}</p>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-11'>
+                          <button
+                            className='social-btns'
+                            onClick={() => handleSaveThread(thread._id)}
+                          >
+                            save
+                          </button>
+                        </div>
+                        <div className='col-1 d-flex align-items-center'>
+                          <p className='comments-count p-2'>{0}</p>
+                          <img
+                            className='comments-btn'
+                            src='/images/comments-regular.svg'
+                            alt='comment button'
+                            onClick={handleCommentBtn}
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className='row'>
-                    <div className='col-12 d-flex pt-2 justify-content-between align-items-center'>
-                      <p>{thread.title}</p>
-                      <p>{formatTimestamp(thread.timestamp)}</p>
-                    </div>
-                  </div>
-                  <div className='row'>
-                    <div className='col-12 content-container'>
-                      <p className='content-desc'>{thread.description}</p>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-12">
-                      <button className='social-btns'>comment</button>
-                      <button className='social-btns' onClick={() => handleSaveThread(thread._id)}>save</button>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                  )
+                )
+              )}
             </div>
           </div>
         </div>
