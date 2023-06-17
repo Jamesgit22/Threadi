7d672495a36d315b6b9a31d8ab3db79d16c7b7af
Jamesgit22 <jschroeder97@icloud.com>
2023-06-17 13:52:11 -0500

    Added delete button to userthreads

--- client/src/components/threadspage/userthreads/UserThreads.jsx
+++ client/src/components/threadspage/userthreads/UserThreads.jsx
@@ -1,19 +1,18 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 import React from 'react';
-import './UserThreads.css'
+import './UserThreads.css';
 
+export default function UserThreads(
+  props,
+  { handlereviewModalTog, getSingleThread }
+) {
+  console.log(typeof props.date);
+  const date = props.date.split(' ');
+  const fDate = date[1] + ' ' + date[2] + ' ' + date[3];
 
-
-export default function UserThreads(props, { handlereviewModalTog, getSingleThread }) {
-
-
-    console.log(typeof(props.date));
-      const date = props.date.split(' ');
-      const fDate = date[1] + ' ' + date[2] + ' ' + date[3];
-    
-    const sendData = () => {
-      props.getSingleThread(props.id);
-    }
+  const sendData = () => {
+    props.getSingleThread(props.id);
+  };
 
   return (
     <>
@@ -31,7 +30,9 @@ export default function UserThreads(props, { handlereviewModalTog, getSingleThre
             <div className='col-12 threads-box p-0'>
               <div className='row'>
                 <div className='col-6'>
-                  <h4 className='thread-description'>{props.description || 'No description'}</h4>
+                  <h4 className='thread-description'>
+                    {props.description || 'No description'}
+                  </h4>
                 </div>
                 <div className='col-6 p-0 m-0'>
                   <img
@@ -47,16 +48,22 @@ export default function UserThreads(props, { handlereviewModalTog, getSingleThre
         </div>
         <div className='row'>
           <div className='col-12 d-flex justify-content-evenly align-items-center thread-btns-container'>
-            <p className='m-0 pt-1 pb-1'>(edit)</p>
-            <button
-              className='thread-open-btn'
-
-              onClick={sendData}
-
-            >
-              Open
-            </button>
-            <p className='m-0 pt-1 pb-1'>(delete)</p>
+            <div className='col-4'>
+              <p className='m-0 pt-1 pb-1'></p>
+            </div>
+            <div className='col-4'>
+              <button className='thread-open-btn' onClick={sendData}>
+                Open
+              </button>
+            </div>
+            <div className='col-4 text-end'>
+              <img
+                src='/images/trash-can-solid.svg'
+                alt='delete button'
+                className='m-0 pt-1 pb-1 trash-can'
+                onClick={console.log('delete')}
+              />
+            </div>
           </div>
         </div>
         {/* end */}
