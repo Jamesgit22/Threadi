77cd1ad143b2569a6db89e5f55007a706cd3ad90
Jamesgit22 <jschroeder97@icloud.com>
2023-06-22 14:49:01 -0500

    Fixed bug with the style on sign in and sign out buttons

--- client/src/components/nav/navbuttons/NavSOButtons.jsx
+++ client/src/components/nav/navbuttons/NavSOButtons.jsx
@@ -5,7 +5,7 @@ import Auth from "../../../utils/auth";
 function NavSOButtons() {
     return (
         
-        <div className="desktopSignOn col-3 d-flex justify-content-end">
+        <div className="desktopSignOn col-12 d-flex justify-content-end">
             <button onClick={Auth.logout} className="desktop-signup-btns me-3">Sign Out</button>
         </div>
     );
