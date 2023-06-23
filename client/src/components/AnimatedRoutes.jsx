1854f8b415af21d6be1b5d2c07913de9d8b1d33d
Jamesgit22 <jschroeder97@icloud.com>
2023-06-23 10:05:09 -0500

    Added fade transition when switching pages. Also created files in profile folder to start working on friends page

--- client/src/components/AnimatedRoutes.jsx
+++ client/src/components/AnimatedRoutes.jsx
@@ -15,18 +15,15 @@ import Loading from './loading/Loading'
 // Define the page transition variants
 const pageVariants = {
   initial: {
-    opacity: 0,
-    x: "-100vw"
+    opacity: 0
   },
   animate: {
-    opacity: 1,
-    x: 0
+    opacity: 1
   },
   exit: {
     opacity: 0,
-    x: "100vw",
     transition: {
-      duration: 0.3
+      duration: 0.5
     }
   }
 };
